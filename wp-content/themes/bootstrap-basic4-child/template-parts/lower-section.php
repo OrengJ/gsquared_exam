9770d20a4f894e82e74400ddc4209cabf9d23a2d
John Leomar Rampola <johnleomar.rampola@gmail.com>
2024-03-14 16:50:02 +0800

    Created the lower section of the page

--- wp-content/themes/bootstrap-basic4-child/template-parts/lower-section.php
+++ wp-content/themes/bootstrap-basic4-child/template-parts/lower-section.php
@@ -1,6 +1,42 @@
 <section class="lower-section">
     <div class="container-fluid">
-        <h2>Lower Section</h2>
-        <p>This is the lower section of the template.</p>
+        <div class="row mx-5 align-items-center">
+            <div class="col-md-1"></div>
+            <div class="col-md-4 text-center image"></div>
+            <div class="col-md-6 mx-5">
+                <div class="container d-flex flex-column justify-content-center">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/symbol-L.svg'; ?>" class="float-right-custom">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/icon-square-dots.svg'; ?>" class="square-image">
+                    </div>
+                    <div class="row mb-4 mx-5">
+                        <p class="title-description text-justify text-wrap">
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam <br/>
+                            nonumy eirmod tempor invidunt ut labore et dolore magna <br/>
+                            aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                        </p>
+                    </div>
+                    <div class="row mx-5">
+                        <p class="description text-justify text-wrap">
+                            With only 3 hospices / respite centres in Australia for children under the age of 18, Rio’s Legacy’s vision is to provide more
+                            facilities readily available for families going through the most difficult time in their lives, living with a child who has a
+                            terminal illness.  On top of this, Rio’s Legacy will look to assist families who have a child in the intensive care unit at the
+                            Sydney Children’s Hospital and support children and young people who have been diagnosed with a terminal illness and
+                            their families at Bear Cottage.
+                        </p>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/symbol-L.svg'; ?>" class="float-left-custom">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
     </div>
 </section>
